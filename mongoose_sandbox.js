85349eefe2cdec07f91aef78ac9a40664356bc12
mishlaev-personal <mishlaev@gmail.com>
2016-10-03 01:45:33 +0300

    added sandbox. playing with mongo

--- mongoose_sandbox.js
+++ mongoose_sandbox.js
@@ -16,18 +16,38 @@ db.once("open", function(){
 
     var Schema = mongoose.Schema;
     var AnimalSchema = new Schema ({
-        size: {type: String, default: "small"},
+        size: String,
         color: {type: String, default: "golden"},
         mass: {type: Number, default: 0.0007},
         type: {type: String, default: "goldfish"},
         name: {type: String, default: "Angela"}
     });
 
+    AnimalSchema.pre("save", function(next) {
+        if (this.mass >= 100) {
+            this.size = "big";
+        } else if (this.mass >= 5 && this.mass < 100) {
+            this.size = "medium";
+        } else {
+            this.size = "small";
+        }
+        next();
+    });
+
+    AnimalSchema.statics.findSize = function(size, callback) {
+        //this == animal
+        return this.find({size: size}, callback);
+    };
+
+    AnimalSchema.methods.findSameColor = function(callback){
+        //this === document
+        return this.model("Animal").find({color: this.color}, callback);
+    };
+
     var Animal = mongoose.model("Animal", AnimalSchema);
 
     var elephant = new Animal({
         type: "elephant",
-        size: "big",
         color: "gray",
         mass: 6000,
         name: "Lawrance"
@@ -37,27 +57,47 @@ db.once("open", function(){
 
     var whale = new Animal({
         type: "whale",
-        size: "big",
         mass: 190500,
         name: "Fig"
     });
 
+    var animalData = [
+        {
+            type: "mouse",
+            color: "gray",
+            mass: 0.0043,
+            name: "Marvin"
+        },
+        {
+            type: "nutria",
+            color: "brown",
+            mass: 4.24,
+            name: "Greetchen"
+        },
+        {
+            type: "wolf",
+            color: "gray",
+            mass: 45,
+            name: "Iris"
+        },
+        elephant,
+        fish,
+        whale
+    ];
+
     Animal.remove({}, function(err) {
         if (err) console.error(err);
-        elephant.save(function(err){
+        Animal.create(animalData, function(err, animals){
             if (err) console.error(err);
-            fish.save(function(err){
-                if (err) console.error(err);
-                whale.save(function(err){
-                    if (err) console.log(err);
-                        Animal.find({size: 'big'}, function(err, animals) {
-                            console.log("Does Aminal.find() works?");
-                            animals.forEach(function(animal){
-                                console.log(animal.name + "the" + animal.color
-                                + " " + animal.type);
-                            });
+            Animal.findOne({type: "elephant"}, function(err, elephant) {
+                console.log("It appered that " + elephant.name + " is "
+                + elephant.color + ". So let's find other same color animals:");
+                elephant.findSameColor(function(err, animals){
+                    animals.forEach(function(animal){
+                        console.log(animal.name + " the " + animal.color
+                        + " " + animal.type + " is " + animal.size
+                        + " (" + animal.mass + " kg)");
                     });
-
                     db.close(function(){
                         console.log("db connection closed");
                     });
@@ -65,7 +105,4 @@ db.once("open", function(){
             });
         });
     });
-
-
-
 });
