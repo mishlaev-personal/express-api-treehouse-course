f4ebe883fc84bbccb14c38580f3e333fcdbfff2d
mishlaev-personal <mishlaev@gmail.com>
2016-10-09 22:12:55 +0300

    Create Models

--- app.js
+++ app.js
@@ -10,6 +10,21 @@ var logger = require("morgan");
 app.use(logger("dev"));
 app.use(jsonParser());
 
+var mongoose=require('mongoose');
+
+mongoose.connect("mongodb://localhost:27017/qa");
+
+var db = mongoose.connection;
+
+db.on('error', function(err){
+    console.error("db connection error: ", err);
+});
+
+db.once("open", function(){
+    console.log("db connection on");
+    // all db comunications here
+});
+
 app.use('/questions', routers);
 
 // catch 404 and forward to error handler
